ac3de26c78c8e9de21eb817e3cc06978062b601a
doctone <samjojames@gmail.com>
2022-01-18 12:30:19 +0000

    Fezz for 13

--- solution.js
+++ solution.js
@@ -1,3 +1,10 @@
+/*
+If a number is a multiple of 13, print "Fezz" instead of the number. 
+For multiples of most other numbers, the Fezz goes immediately in front of the first thing beginning with B, 
+or at the end if there are none. (E.g. 5 * 13 = 65: "FezzBuzz", 3 * 5 * 13 = 195: "FizzFezzBuzz"). 
+Note that Fezz should be printed even if Bong is also present (E.g. 11 * 13 = 143: "FezzBong")
+*/
+
 for (let i=1; i<=100; i++){
     let answer = '';
     if (i % 3 === 0){
@@ -11,6 +18,15 @@ for (let i=1; i<=100; i++){
     }
     if (i % 11 === 0){
         answer = 'Bong';
+    }
+    if (i % 13 === 0){
+        let index = answer.indexOf('B');
+        if (index < 0){
+            answer = answer + 'Fezz';
+        }
+        else {
+            answer = answer.slice(0, index) + 'Fezz' + answer.slice(index);
+        }
     }
     // check if empty
     if (!answer) answer = i;
