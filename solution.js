3a08c3f364072fdfdc45321492a5eaf171feb450
Sam James <Sam.James@softwire.com>
2022-01-18 10:50:49 +0000

    added 7s and 11s

--- solution.js
+++ solution.js
@@ -6,6 +6,12 @@ for (let i=1; i<=100; i++){
     if (i % 5 === 0){
         answer += 'Buzz';
     }
+    if ( i % 7 === 0){
+        answer += 'Bang';
+    }
+    if (i % 11 === 0){
+        answer = 'Bong';
+    }
     // check if empty
     if (!answer) answer = i;
     console.log(answer);
