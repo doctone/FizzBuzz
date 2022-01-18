8b387a06411a716943d23d5e197ac0945e5d8a31
doctone <samjojames@gmail.com>
2022-01-18 20:27:22 +0000

    improved code and completed all but 13

--- solution.js
+++ solution.js
@@ -1,37 +1,32 @@
-/*
-If a number is a multiple of 13, print "Fezz" instead of the number. 
-For multiples of most other numbers, the Fezz goes immediately in front of the first thing beginning with B, 
-or at the end if there are none. (E.g. 5 * 13 = 65: "FezzBuzz", 3 * 5 * 13 = 195: "FizzFezzBuzz"). 
-Note that Fezz should be printed even if Bong is also present (E.g. 11 * 13 = 143: "FezzBong")
-*/
+function isDivisibleBy(a, b){
+    return a%b === 0;
+}
 
-for (let i=1; i<=100; i++){
-    let answer = '';
-    if (i % 3 === 0){
-        answer += 'Fizz';
+for (let i=1; i<=255; i++){
+    let answer = [];
+    if (isDivisibleBy(i, 3)){
+        answer.push('Fizz');
     }
-    if (i % 5 === 0){
-        answer += 'Buzz';
+    if (isDivisibleBy(i, 5)){
+        answer.push('Buzz');
     }
-    if ( i % 7 === 0){
-        answer += 'Bang';
+    if (isDivisibleBy(i, 7)){
+        answer.push('Bang');
     }
-    if (i % 11 === 0){
-        answer = 'Bong';
+    if (isDivisibleBy(i, 11)){
+        answer = ['Bong'];
     }
-    if (i % 13 === 0){
-        // find 'B'
-        let index = answer.indexOf('B');
-        // if there's no 'B' word, put 'Fezz' on the end
-        if (index < 0){
-            answer += 'Fezz';
-        }
-        // otherwise, insert 'Fezz' before the 'B' word
-        else {
-            answer = answer.slice(0, index) + 'Fezz' + answer.slice(index);
-        }
+    if (isDivisibleBy(i, 13)){
+        /// TO DO
     }
+
+    if (isDivisibleBy(i, 17)){
+        answer.reverse();
+    }
+
     // check if empty
-    if (!answer) answer = i;
-    console.log(answer);
+    if (!answer.length) {
+        answer = [i.toString()];
+    }
+    console.log(answer.join(''));
 }
