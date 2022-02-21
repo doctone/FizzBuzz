60563c15c38d73d6bf4215ff16df0ab56534c6bf
Sam James <samjojames@gmail.com>
2022-02-21 11:32:23 +0000

    Print Fizz, Buzz, and FizzBuzz correctly

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             for (int i=1; i<=100; i++){
-                Console.WriteLine(i);
+                string output = "";
+
+                if (i % 3 == 0){
+                    output += "Fizz";
+                }
+                if (i % 5 == 0){
+                    output += "Buzz";
+                }
+                if (output == ""){
+                    output += i.ToString();
+                }
+                Console.WriteLine(output);
             }
         }
     }
