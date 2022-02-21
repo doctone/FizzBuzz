1fb242f629c43e39630ff94d16b8ce138ae11964
Sam James <samjojames@gmail.com>
2022-02-21 12:04:58 +0000

    Rename helper function to match c# conventions

--- FizzBuzzer.cs
+++ FizzBuzzer.cs
@@ -4,23 +4,23 @@ namespace FizzBuzz
 {
     public class FizzBuzzer
     {
-        static bool isDivisibleBy(int a, int b){
+        static bool IsDivisibleBy(int a, int b){
             return a % b == 0;
         }
         public string FizzBuzz(int number)
         {
             string output = "";
 
-            if (isDivisibleBy(number, 3)){
+            if (IsDivisibleBy(number, 3)){
                 output += "Fizz";
             }
-            if (isDivisibleBy(number, 5)){
+            if (IsDivisibleBy(number, 5)){
                 output += "Buzz";
             }
-            if (isDivisibleBy(number, 7)){
+            if (IsDivisibleBy(number, 7)){
                 output += "Bang";
             }
-            if (isDivisibleBy(number, 11)){
+            if (IsDivisibleBy(number, 11)){
                 output = "Bong";
             }
             if (output == ""){
